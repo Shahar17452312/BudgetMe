71914f6d5040422e5ea2fba037bd33e294d955a6
shahar <shaharnaim3@gmail.com>
2025-02-02 17:47:34 +0200

    add test with selenium

---
+++ frontend/test/test.js
@@ -0,0 +1,20 @@
+import RegisterPageTest from "./utils.js";
+import { Builder, Browser,until,By } from "selenium-webdriver";
+
+async function startBot() {
+    const driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+    try {
+        await driver.get("http://localhost:5173");
+        const register=await driver.wait(until.elementLocated(By.css("button[class='register-btn']")));
+        await register.click();
+        await RegisterPageTest(driver,"shahar","123@gmail.com","123","50");
+
+    } catch (e) {
+        console.log("Error opening website: " + e.message);
+    } finally {
+        await driver.quit();
+    }
+}
+
+startBot();
---
+++ frontend/test/utils.js
@@ -0,0 +1,16 @@
+import { By, until, } from "selenium-webdriver";
+
+async function RegisterPageTest(driver,username,email,password,budget){
+  try{
+    await driver.wait(until.elementLocated(By.name("username"))).sendKeys(username);
+    await driver.wait(until.elementLocated(By.name("email"))).sendKeys(email);
+    await driver.wait(until.elementLocated(By.name("password"))).sendKeys(password);
+    await driver.wait(until.elementLocated(By.name("budget"))).sendKeys(budget);
+    await driver.wait(until.elementLocated(By.css("button[class='register-btn']"))).click()
+  }
+  catch(error){
+    console.error("error: ",error.message);
+  }
+}
+
+export default RegisterPageTest;
